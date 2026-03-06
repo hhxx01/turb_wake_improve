97e8695cca60b7c3d4d2b2817fdb244bba611415
hhxx01 <wei17612940836@163.com>
2026-03-06 15:03:51 +0800

    1. 密度修正  2.数据按风速 风向分组

--- constans.py
+++ constans.py
@@ -1,3 +1,10 @@
 
 TURB_NUM = 20
-TURB_ATTRIBUTES = 5
+TURB_ATTRIBUTES_OLD = ['风速_', '风向_', '功率_', '运行状态_', '限功率状态_']
+TURB_ATTRIBUTES_NEW = ['_position', '_power', '_turbine_status', '_wind_direction1', '_wind_speed']
+
+
+
+
+
+
